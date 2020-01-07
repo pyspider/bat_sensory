828666511698cf6afe603d788b626d6a2c940a16
lee <lee.unseo17@gmail.com>
2020-01-07 18:09:20 +0900

    scratch dqn

--- agents/dqn/dqn.py
+++ agents/dqn/dqn.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 import torch
 from torch import nn
 from torch import optim
@@ -7,8 +8,21 @@ from ..replay_memory import *
 
 BATCH_SIZE = 32
 CAPACITY = 10000
-GAMMA = 0.999
+GAMMA = 0.99
 
+
+class Net(nn.Module):
+    def __init__(self, n_in, n_mid, n_out):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(n_in, n_mid)
+        self.fc2 = nn.Linear(n_mid, n_mid)
+        self.fc3 = nn.Linear(n_mid, n_out)
+    
+    def forward(self, x):
+        h1 = F.relu(self.fc1(x))
+        h2 = F.relu(self.fc2(h1))
+        output = self.fc3(h2)
+        return output
 
 class Brain(object):
     def __init__(self, num_states, num_actions):
@@ -16,23 +30,33 @@ class Brain(object):
 
         self.memory = ReplayMemory(CAPACITY)
 
-        self.model = nn.Sequential(
-            nn.Linear(num_states, 32),
-            nn.ReLU(),
-            nn.Linear(32, 32),
-            nn.ReLU(),
-            nn.Linear(32, num_actions))
-
+        n_in, n_mid, n_out = num_states, 32, num_actions
+        self.main_q_network = Net(n_in, n_mid, n_out)
+        self.target_q_network = Net(n_in, n_mid, n_out)
         print('Q-Network')
-        print(self.model)
+        print(self.main_q_network)
 
-        self.optimizer = optim.Adam(self.model.parameters(), lr=0.0001)
+        self.optimizer = optim.Adam(
+            self.main_q_network.parameters(), lr=0.0001)
 
     def replay(self):
         # do nothing when memory is small
         if len(self.memory) < BATCH_SIZE:
             return
         
+        (self.batch,
+         self.state_batch,
+         self.action_batch,
+         self.reward_batch,
+         self.non_final_next_states
+        ) = self.make_minibatch()
+         
+        self.expected_state_action_values = self.get_expected_state_action_values()
+
+        self.update_main_q_network()
+
+   
+    def make_minibatch(self):
         # sample mini-batch from memory
         transitions = self.memory.sample(BATCH_SIZE)
 
@@ -47,45 +71,69 @@ class Brain(object):
         non_final_next_states = torch.cat([
             s for s in batch.next_state if s is not None])
         
+        return batch, state_batch, action_batch, reward_batch, non_final_next_states
+    
+    def get_expected_state_action_values(self):
         # switch network mode to evaluate
-        self.model.eval()
+        self.main_q_network.eval()
+        self.target_q_network.eval()
 
         # calculate Q(s_t, a_t)
-        state_action_values = self.model(state_batch).gather(1, action_batch)
+        self.state_action_values = self.main_q_network(
+            self.state_batch).gather(1, self.action_batch)
 
         # index mask for checking whether there is next state 
-        non_final_mask = torch.ByteTensor(
-            tuple(map(lambda s: s is not None, batch.next_state)))
+        non_final_mask = torch.tensor(
+            tuple(map(lambda s: s is not None, self.batch.next_state)), 
+            dtype=torch.bool)
         
         # initilize to zero
         next_state_values = torch.zeros(BATCH_SIZE) 
 
+        a_m = torch.zeros(BATCH_SIZE).type(torch.LongTensor)
+
         # calculate max Q
-        next_state_values[non_final_mask] = self.model(
-            non_final_next_states).max(1)[0].detach()
+        a_m[non_final_mask] = self.main_q_network(
+            self.non_final_next_states).max(1)[1].detach()
+        
+        # choose states have next_state
+        a_m_non_final_next_states = a_m[non_final_mask].view(-1, 1)
+
+        # calculate target Q
+        next_state_values[non_final_mask] = self.target_q_network(
+            self.non_final_next_states).gather(1, a_m_non_final_next_states
+            ).detach().squeeze()
 
         # calculate Q(s_t, a_t)
-        expected_state_action_values = reward_batch + GAMMA * next_state_values
+        expected_state_action_values = self.reward_batch + GAMMA * next_state_values
 
-        # switch network mode to train
-        self.model.train()
+        return expected_state_action_values
+    
+    def update_main_q_network(self):
+        # switch network to train
+        self.main_q_network.train()
 
         # calculate loss
         loss = F.smooth_l1_loss(
-            state_action_values, expected_state_action_values.unsqueeze(1))
-        
+            self.state_action_values,
+            self.expected_state_action_values.unsqueeze(1))
+        # print('loss:', loss.item())
+
         # update weights
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
+    
+    def update_target_q_network(self):
+        self.target_q_network.load_state_dict(self.main_q_network.state_dict())
 
     def decide_action(self, state, episode):
         epsilon = 0.5 * (1 / (episode + 1))
 
         if epsilon <= np.random.uniform(0, 1):
-            self.model.eval()
+            self.main_q_network.eval()
             with torch.no_grad():
-                action = self.model(state).max(1)[1].view(1, 1)
+                action = self.main_q_network(state).max(1)[1].view(1, 1)
         else:
             action = torch.LongTensor(
                 [[random.randrange(self.num_actions)]])
@@ -100,9 +148,12 @@ class Agent(object):
     def update_q_function(self):
         self.brain.replay()
     
-    def get_action(self):
-        action = self.brain.dicide_action(state, episode)
+    def get_action(self, state, episode):
+        action = self.brain.decide_action(state, episode)
         return action
     
     def memorize(self, state, action, state_next, reward):
         self.brain.memory.push(state, action, state_next, reward)
+    
+    def update_target_q_function(self):
+        self.brain.update_target_q_network()
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import math
+
+import numpy as np
 import gym
 from gym import wrappers
-
 import cv2
 
 import environments
-from agents.dqn import *
+from agents.dqn.dqn import *
 
 # for debug
 from gym import logger
-logger.set_level(10)
+# logger.set_level(10)
 
 
 TIME_STEP = 0.005
@@ -51,35 +52,71 @@ def train_dqn():
 
     # env = gym.make('LidarBatEnv-v0')
     env = gym.make('CartPole-v0')
-    env = wrappers.Monitor(env, './videos/dqn')
+    env = wrappers.Monitor(env, './videos/dqn', force=True)
 
-    observation = env.reset()
-    num_states = observation.n
-    agent = Agent()
+    num_states = env.observation_space.shape[0]
+    num_actions = env.action_space.n
+    agent = Agent(num_states, num_actions)
+
+    episode_10_list = np.zeros(10)
+    complete_episodes = 0
+    episode_final = False
 
     for i_episode in range(500):
         observation = env.reset()
-        for t in range(200):
-            print(f'----step {t}----')
-            print(f'bat angle: {env.bat.angle *180 / math.pi:2f} [degree]')
-            print('observation:')
-            print(observation)
-            action = env.action_space.sample()
+        state = observation
+        state = torch.from_numpy(state).type(torch.FloatTensor)
+        state = torch.unsqueeze(state, 0)
+        for step in range(200):
+            # print(f'----step {step}----')
+            # print(f'bat angle: {env.bat.angle *180 / math.pi:2f} [degree]')
+            # print('observation:')
+            # print(observation)
 
+            action = agent.get_action(state, i_episode)
+            # print(f'action: {action}')
 
-            print(f'action: {action}')
-            observation, reward, done, info = env.step(action)
-            print(f'reward: {reward}')
-            print(f'done: {done}')
-            print(f'time: {env.t:2f} [sec]')
+            observation_next, _, done, _ = env.step(action.item())
+            # print(f'reward: {reward}')
+            # print(f'done: {done}')
+            # print(f'time: {env.t:2f} [sec]')
             if done:
-                print(f"Episode finished after {t+1} timesteps")
+                print(f"Episode finished after {step+1} timesteps")
+                state_next = None
+                episode_10_list = np.hstack((episode_10_list[1:], step+1))
+
+                if step < 195:
+                    reward = torch.FloatTensor([-1.0])
+                    complete_episodes = 0
+                else:
+                    reward = torch.FloatTensor([1.0])
+                    complete_episodes = complete_episodes + 1
+            else:
+                reward = torch.FloatTensor([0.0])
+                state_next = observation_next
+                state_next = torch.from_numpy(state_next).type(torch.FloatTensor)
+                state_next = torch.unsqueeze(state_next, 0)
+
+            agent.memorize(state, action, state_next, reward)
+            agent.update_q_function()
+            state = state_next
+
+            if done:
+                print(f'{i_episode} Episode: Finished after {step+1} steps: average 10 steps = {episode_10_list.mean()}')
+
+                if (i_episode % 2 == 0):
+                    agent.update_target_q_function()
                 break
+
+        if complete_episodes >= 10:
+            print('Clear 10 times in a row')
+            break
+
         env.reset()
     env.close()
 
 
 
 if __name__ == "__main__":
-    # train_dqn()
-    main()
+    train_dqn()
+    # main()
